8fd7e7bbc230127487e8f3ffb9bb5cff2125397e
kambanite <kambanite@gmail.com>
2017-04-17 19:47:12 +0300

    better now

--- analysis.R
+++ analysis.R
@@ -44,11 +44,11 @@ q7q8clms = clm.each(q7q8nona) # not a single good model (not surprisingly)
 q7q8clms.evals = lapply(q7q8clms, eval.model)
 
 # naive models, predict q8 with q8 & FA(q7)
-naivedf = cbind(q7q8nona[, grepl("q8", names(q7q8nona))], q7q8fa0$scores)
-naive1 = clm.each(naivedf,link = "logit") # q8.5_beentertained
-naive2 = clm.each(naivedf,link = "probit") # q8.5_beentertained
-naive3 = clm.each(naivedf,link = "cauchit") # nothing
-naive4 = clm.each(naivedf,link = "cloglog") # q8.5_beentertained
+# naivedf = cbind(q7q8nona[, grepl("q8", names(q7q8nona))], q7q8fa0$scores)
+# naive1 = clm.each(naivedf,link = "logit") # q8.5_beentertained
+# naive2 = clm.each(naivedf,link = "probit") # q8.5_beentertained
+# naive3 = clm.each(naivedf,link = "cauchit") # nothing
+# naive4 = clm.each(naivedf,link = "cloglog") # q8.5_beentertained
 
 # less naive models, predict q8 with q7
 pred1 = q7q8nona[,grepl("q7",names(q7q8nona))]
@@ -59,21 +59,27 @@ lessnaive3 = model.list(x = pred1, y = resp1, link = "cauchit") # nothing
 lessnaive4 = model.list(x = pred1, y = resp1, link = "cloglog") # q8.1_killtime, q8.5_beentertained, q8.4_browsenoreason
 
 
-# Q10 ---------------------------------------------------------------------
-# H0: single latent factor - nope
-q10fa = brute.force.fa(q10nonanum)
-q10fa = factanal(q10nonanum, factors = 3)
 
-# H1: q10 ~ q11
-q10q11 = cbind(q10,q11)
-q10q11nona = na.omit(q10q11)
-q10q11nonanum = fact2num(q10q11nona)
-
-pred = q10q11nona[,grepl("q11",names(q10q11nona))]
-resp = q10q11nona[,grepl("q10",names(q10q11nona))]
-q10m1 = model.list(x = pred, y = resp, link = "logit")
-q10m2 = clm.each(q10q11nona, link = "cloglog") # logit nope, probit nope, cauchit nope, cloglog - q10.3_browsephoto
+# Evaluate the above models -----------------------------------------------
+par(mfrow=c(2,2))
+my.barplot(unlist(lapply(lessnaive1,function(x) eval.model(x)$total)),
+           namez = names(lessnaive1),main="logit")
+my.barplot(unlist(lapply(lessnaive2,function(x) eval.model(x)$total)),
+           namez = names(lessnaive2),main="probit")
+my.barplot(unlist(lapply(lessnaive3,function(x) eval.model(x)$total)),
+           namez = names(lessnaive3),main="cauchit")
+my.barplot(unlist(lapply(lessnaive4,function(x) eval.model(x)$total)),
+           namez = names(lessnaive4),main="cloglog")
 
 
-
+plot(unlist(lapply(lessnaive1,function(x) eval.model(x)$total)),type="l",col=1
+     ,ylim=c(.3,1),ylab="Accuracy",xaxt="n")
+lines(unlist(lapply(lessnaive2,function(x) eval.model(x)$total)),col=2)
+lines(unlist(lapply(lessnaive3,function(x) eval.model(x)$total)),col=3)
+lines(unlist(lapply(lessnaive4,function(x) eval.model(x)$total)),col=4)
+grid(10,2)
+text(seq(1,length(lessnaive1),len=length(lessnaive1)),
+     rep(par("usr")[3],length(lessnaive1)),
+     labels=names(lessnaive1),xpd=NA,srt=45)
+legend("top",horiz=T,c("logit","probit","cauchit","cloglog"),bty="n",fill=1:4)
 
